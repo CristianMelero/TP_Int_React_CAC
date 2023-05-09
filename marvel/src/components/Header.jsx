53fa132808708b0f249e84f49a7586de9f11c22a
celerrodriguez <celesteerrodriguez@gmail.com>
2023-05-09 17:28:53 -0300

    carpeta component + footer y header
    Cree la carpeta y apenas comence con una idea pequeña en header

---
+++ marvel/src/components/Header.jsx
@@ -0,0 +1,26 @@
+
+
+
+export const Header = ()=>{
+
+    return(
+        <>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Juego</Link>
+                    </li>
+                </ul>
+                <button>Log in</button>
+                <Link>Sign up</Link>
+            </nav>
+        </header>
+        
+        
+        </>
+    )
+}

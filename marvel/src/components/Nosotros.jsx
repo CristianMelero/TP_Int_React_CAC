e637f00a700ac53a580b796cfd85bfef59c836d5
Cristian Melero <cristianmelero22@yahoo.com.ar>
2023-07-15 23:23:08 -0300

    Agrego perfil en nos

--- marvel/src/components/Nosotros.jsx
+++ marvel/src/components/Nosotros.jsx
@@ -1,76 +1,105 @@
-import { Card, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const Nosotros = () => {
 	return (
 		<div className="main">
-			<h1 className='titulo'>
-				¿Quiénes somos?
-			</h1>
+			<h1 className="titulo">¿Quiénes somos?</h1>
 
-			<p className="nosotrosTexto">Este proyecto fue creado como trabajo integrador grupal por estudiantes del curso <b style={{ color: 'brown' }}>Codo a Codo 4.0: React</b> <i>(comisión 23307)</i> instruido por el profesor Gabriel Muñoz, en el primer cuatrimestre de 2023.
+			<p className="nosotrosTexto">
+				Este proyecto fue creado como trabajo integrador grupal por
+				estudiantes del curso{" "}
+				<b style={{ color: "brown" }}>Codo a Codo 4.0: React</b>{" "}
+				<i>(comisión 23307)</i> instruido por el profesor Gabriel Muñoz,
+				en el primer cuatrimestre de 2023.
 				<br></br>
 				Los desarrolladores que lograron crear esta página son:
 			</p>
 
 			<div className="nosotrosCards">
-				<Card style={{ width: '20rem' }}>
-					<Card.Img variant="top" src="" />
+				<Card style={{ width: "20rem" }}>
+					<Card.Img
+						variant="top"
+						src="https://media.licdn.com/dms/image/D4D35AQEB9S-8Ehk-lA/profile-framedphoto-shrink_400_400/0/1677672236035?e=1690081200&v=beta&t=N5MNwIbAvDszSS3HD_7Uj9I_CL9L0_nIkOIFrt5XqaA"
+					/>
 					<Card.Body>
 						<Card.Title>Josefina Sofio Avogadro</Card.Title>
 						<Card.Text>
-							Diseñadora Multimedial & Comunicación digital e interactiva.
+							Diseñadora Multimedial & Comunicación digital e
+							interactiva.
 						</Card.Text>
-						<Link to="https://www.linkedin.com/in/josefinasofio/"><Button variant="danger">LinkedIn</Button></Link>
-						<Link to="https://github.com/jsofio"><Button variant="danger">Github</Button></Link>
+						<Link to="https://www.linkedin.com/in/josefinasofio/">
+							<Button variant="danger">LinkedIn</Button>
+						</Link>
+						<Link to="https://github.com/jsofio">
+							<Button variant="danger">Github</Button>
+						</Link>
 					</Card.Body>
 				</Card>
 
-				<Card style={{ width: '20rem' }}>
+				<Card style={{ width: "20rem" }}>
+					<Card.Img
+						variant="top"
+						src="https://avatars.githubusercontent.com/u/112022207?v=4"
+						alt="Foto de Cristian Melero"
+					/>
+					<Card.Body>
+						<Card.Title>Cristian Abel Melero</Card.Title>
+						<Card.Text>Desarrollador FrontEnd con conocimientos en BackEnd</Card.Text>
+						<Link to="https://www.linkedin.com/in/crismelero/">
+							<Button variant="danger">Linkedin</Button>
+						</Link>
+						<Link to="https://github.com/CristianMelero">
+							<Button variant="danger">Github</Button>
+						</Link>
+					</Card.Body>
+				</Card>
+
+				<Card style={{ width: "20rem" }}>
 					<Card.Img variant="top" src="" />
 					<Card.Body>
 						<Card.Title>Nombre</Card.Title>
 						<Card.Text>
 							Puesto IT, descripción o rol en el proyecto
 						</Card.Text>
-						<Link to="/"><Button variant="danger">Enlace</Button></Link>
-						<Link to="/"><Button variant="danger">Enlace</Button></Link>
+						<Link to="/">
+							<Button variant="danger">Enlace</Button>
+						</Link>
+						<Link to="/">
+							<Button variant="danger">Enlace</Button>
+						</Link>
 					</Card.Body>
 				</Card>
 
-				<Card style={{ width: '20rem' }}>
+				<Card style={{ width: "20rem" }}>
 					<Card.Img variant="top" src="" />
 					<Card.Body>
 						<Card.Title>Nombre</Card.Title>
 						<Card.Text>
 							Puesto IT, descripción o rol en el proyecto
 						</Card.Text>
-						<Link to="/"><Button variant="danger">Enlace</Button></Link>
-						<Link to="/"><Button variant="danger">Enlace</Button></Link>
+						<Link to="/">
+							<Button variant="danger">Enlace</Button>
+						</Link>
+						<Link to="/">
+							<Button variant="danger">Enlace</Button>
+						</Link>
 					</Card.Body>
 				</Card>
 
-				<Card style={{ width: '20rem' }}>
+				<Card style={{ width: "20rem" }}>
 					<Card.Img variant="top" src="" />
 					<Card.Body>
 						<Card.Title>Nombre</Card.Title>
 						<Card.Text>
 							Puesto IT, descripción o rol en el proyecto
 						</Card.Text>
-						<Link to="/"><Button variant="danger">Enlace</Button></Link>
-						<Link to="/"><Button variant="danger">Enlace</Button></Link>
-					</Card.Body>
-				</Card>
-
-				<Card style={{ width: '20rem' }}>
-					<Card.Img variant="top" src="" />
-					<Card.Body>
-						<Card.Title>Nombre</Card.Title>
-						<Card.Text>
-							Puesto IT, descripción o rol en el proyecto
-						</Card.Text>
-						<Link to="/"><Button variant="danger">Enlace</Button></Link>
-						<Link to="/"><Button variant="danger">Enlace</Button></Link>
+						<Link to="/">
+							<Button variant="danger">Enlace</Button>
+						</Link>
+						<Link to="/">
+							<Button variant="danger">Enlace</Button>
+						</Link>
 					</Card.Body>
 				</Card>
 			</div>
